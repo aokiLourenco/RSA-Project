1351f5bb0cef414fba0da03b5011c00f8b752be2
yeusef0202 <ant.almeida@ua.pt>
2025-05-27 12:12:57 +0100

    More stuff

--- YOLOStuff/ROS/rosWay/launch_tracking_sensor.sh
+++ YOLOStuff/ROS/rosWay/launch_tracking_sensor.sh
@@ -9,6 +9,8 @@ docker run \
     --env DISPLAY=$DISPLAY \
     --env QT_X11_NO_MITSHM=1 \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env FASTRTPS_DEFAULT_PROFILES_FILE=/config/fastdds.xml \
+    --volume="fastdds.xml:/config/fastdds.xml" \
     --device /dev/video0:/dev/video0 \
     --device /dev/video1:/dev/video1 \
     -p 5000:5000 \
